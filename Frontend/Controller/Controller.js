363bf37b2476e7ee2cd1e48bc77e766af4a4988c
Ferenc Antal <af@dorsum.eu>
2024-01-12 22:07:01 +0100

    Data fetch, console out put

--- Frontend/Controller/Controller.js
+++ Frontend/Controller/Controller.js
@@ -1,6 +1,12 @@
-export default class Controller{
-    constructor(){
-        
-    }
+import DataService from "../Model/DataService.js";
 
+export default class Controller {
+  constructor() {
+    this.dataService = new DataService();
+    this.dataService.getData("investment", this.megjelenit);
+  }
+
+  megjelenit(list) {
+    console.log(list);
+  }
 }
